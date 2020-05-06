ef954fd5731e05787f126fc51937f69ac68f9fb7
schmidtb2779 <schmidtb2779@my.uwstout.edu>
2020-05-06 18:01:01 -0500

    Note data insert

---
+++ sql_scripts/Note.sql
@@ -0,0 +1,10 @@
+INSERT INTO `G7AgileExperience`.`Note` (`noteID`, 'noteText', 'graderID', 'noteSheetID') VALUES ('1', 'Test Note 1', '137', '1');
+INSERT INTO `G7AgileExperience`.`Note` (`noteID`, 'noteText', 'graderID', 'noteSheetID') VALUES ('2', 'Test Note 2', '143', '1');
+INSERT INTO `G7AgileExperience`.`Note` (`noteID`, 'noteText', 'graderID', 'noteSheetID') VALUES ('3', 'Test Note 1', '149', '2');
+INSERT INTO `G7AgileExperience`.`Note` (`noteID`, 'noteText', 'graderID', 'noteSheetID') VALUES ('4', 'Test Note 2', '137', '2');
+INSERT INTO `G7AgileExperience`.`Note` (`noteID`, 'noteText', 'graderID', 'noteSheetID') VALUES ('5', 'Test Note 1', '143', '3');
+INSERT INTO `G7AgileExperience`.`Note` (`noteID`, 'noteText', 'graderID', 'noteSheetID') VALUES ('6', 'Test Note 2', '149', '3');
+INSERT INTO `G7AgileExperience`.`Note` (`noteID`, 'noteText', 'graderID', 'noteSheetID') VALUES ('7', 'Test Note 1', '137', '4');
+INSERT INTO `G7AgileExperience`.`Note` (`noteID`, 'noteText', 'graderID', 'noteSheetID') VALUES ('8', 'Test Note 2', '143', '4');
+INSERT INTO `G7AgileExperience`.`Note` (`noteID`, 'noteText', 'graderID', 'noteSheetID') VALUES ('9', 'Test Note 1', '149', '5');
+INSERT INTO `G7AgileExperience`.`Note` (`noteID`, 'noteText', 'graderID', 'noteSheetID') VALUES ('10', 'Test Note 2', '137', '6');
