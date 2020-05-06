61edbdf45073f9d50388174f3834a3e8c45b69b0
schmidtb2779 <schmidtb2779@my.uwstout.edu>
2020-05-06 17:18:47 -0500

    EnrollsIn and In/Complete data scripts

---
+++ sql_scripts/CompleteIncomplete.sql
@@ -0,0 +1,5 @@
+INSERT INTO `G7AgileExperience`.`CompleteIncomplete` (`gradeID`, `assignedValue`) VALUES ('1', 'Incomplete');
+INSERT INTO `G7AgileExperience`.`CompleteIncomplete` (`gradeID`, `assignedValue`) VALUES ('2', 'Complete');
+INSERT INTO `G7AgileExperience`.`CompleteIncomplete` (`gradeID`, `assignedValue`) VALUES ('3', 'Incomplete');
+INSERT INTO `G7AgileExperience`.`CompleteIncomplete` (`gradeID`, `assignedValue`) VALUES ('4', 'Incomplete');
+INSERT INTO `G7AgileExperience`.`CompleteIncomplete` (`gradeID`, `assignedValue`) VALUES ('5', 'Complete');
---
+++ sql_scripts/EnrollsIn.sql
@@ -0,0 +1,6 @@
+INSERT INTO `G7AgileExperience`.`EnrollsIN` (`studentID`, `sectionID`, 'classID') VALUES ('12345', '1', '1');
+INSERT INTO `G7AgileExperience`.`EnrollsIN` (`studentID`, `sectionID`, 'classID') VALUES ('12453', '1', '2');
+INSERT INTO `G7AgileExperience`.`EnrollsIN` (`studentID`, `sectionID`, 'classID') VALUES ('12561', '2', '1');
+INSERT INTO `G7AgileExperience`.`EnrollsIN` (`studentID`, `sectionID`, 'classID') VALUES ('12669', '2', '2');
+INSERT INTO `G7AgileExperience`.`EnrollsIN` (`studentID`, `sectionID`, 'classID') VALUES ('12777', '3', '1');
+INSERT INTO `G7AgileExperience`.`EnrollsIN` (`studentID`, `sectionID`, 'classID') VALUES ('12885', '3', '2');
