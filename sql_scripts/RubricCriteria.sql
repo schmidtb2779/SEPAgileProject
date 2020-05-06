c614677f2fe07a65180aeb8abe1f306444d12634
schmidtb2779 <schmidtb2779@my.uwstout.edu>
2020-05-06 17:44:02 -0500

    RubricCriteria data script

---
+++ sql_scripts/RubricCriteria.sql
@@ -0,0 +1,11 @@
+INSERT INTO `G7AgileExperience`.`RubricCriteria` ('ID', `rubricID`, 'description', `maxPoints`) VALUES ('1', '1', 'description 1', '15');
+INSERT INTO `G7AgileExperience`.`RubricCriteria` ('ID', `rubricID`, 'description', `maxPoints`) VALUES ('2', '1', 'description 2', '15');
+INSERT INTO `G7AgileExperience`.`RubricCriteria` ('ID', `rubricID`, 'description', `maxPoints`) VALUES ('3', '1', 'description 3', '15');
+INSERT INTO `G7AgileExperience`.`RubricCriteria` ('ID', `rubricID`, 'description', `maxPoints`) VALUES ('4', '2', 'description 1', '25');
+INSERT INTO `G7AgileExperience`.`RubricCriteria` ('ID', `rubricID`, 'description', `maxPoints`) VALUES ('5', '2', 'description 2', '25');
+INSERT INTO `G7AgileExperience`.`RubricCriteria` ('ID', `rubricID`, 'description', `maxPoints`) VALUES ('6', '3', 'description 1', '10');
+INSERT INTO `G7AgileExperience`.`RubricCriteria` ('ID', `rubricID`, 'description', `maxPoints`) VALUES ('7', '3', 'description 2', '10');
+INSERT INTO `G7AgileExperience`.`RubricCriteria` ('ID', `rubricID`, 'description', `maxPoints`) VALUES ('8', '4', 'description 1', '15');
+INSERT INTO `G7AgileExperience`.`RubricCriteria` ('ID', `rubricID`, 'description', `maxPoints`) VALUES ('9', '4', 'description 2', '15');
+INSERT INTO `G7AgileExperience`.`RubricCriteria` ('ID', `rubricID`, 'description', `maxPoints`) VALUES ('10', '5', 'description 1', '15');
+INSERT INTO `G7AgileExperience`.`RubricCriteria` ('ID', `rubricID`, 'description', `maxPoints`) VALUES ('11', '5', 'description 2', '15');
