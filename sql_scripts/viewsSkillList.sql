ae2acf6e2d4926c347b8a5a96c5a3d53180f7ded
schmidtb2779 <schmidtb2779@my.uwstout.edu>
2020-05-06 17:56:02 -0500

    ViewsSkillList data insert

---
+++ sql_scripts/viewsSkillList.sql
@@ -0,0 +1,6 @@
+INSERT INTO `G7AgileExperience`.`ViewsSkillList` (`siID`, `skillListID`) VALUES ('18', '1');
+INSERT INTO `G7AgileExperience`.`ViewsSkillList` (`siID`, `skillListID`) VALUES ('29', '2');
+INSERT INTO `G7AgileExperience`.`ViewsSkillList` (`siID`, `skillListID`) VALUES ('117', '3');
+INSERT INTO `G7AgileExperience`.`ViewsSkillList` (`siID`, `skillListID`) VALUES ('40', '1');
+INSERT INTO `G7AgileExperience`.`ViewsSkillList` (`siID`, `skillListID`) VALUES ('51', '2');
+INSERT INTO `G7AgileExperience`.`ViewsSkillList` (`siID`, `skillListID`) VALUES ('62', '3');
