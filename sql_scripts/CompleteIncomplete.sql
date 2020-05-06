d88b62e2f24c8fd73909706c9cfd73769ca3cd58
schmidtb2779 <schmidtb2779@my.uwstout.edu>
2020-05-06 17:38:32 -0500

    Removed Complete/Incomplete - data should be generated

--- sql_scripts/CompleteIncomplete.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `G7AgileExperience`.`CompleteIncomplete` (`gradeID`, `assignedValue`) VALUES ('1', 'Incomplete');
-INSERT INTO `G7AgileExperience`.`CompleteIncomplete` (`gradeID`, `assignedValue`) VALUES ('2', 'Complete');
-INSERT INTO `G7AgileExperience`.`CompleteIncomplete` (`gradeID`, `assignedValue`) VALUES ('3', 'Incomplete');
-INSERT INTO `G7AgileExperience`.`CompleteIncomplete` (`gradeID`, `assignedValue`) VALUES ('4', 'Incomplete');
-INSERT INTO `G7AgileExperience`.`CompleteIncomplete` (`gradeID`, `assignedValue`) VALUES ('5', 'Complete');
