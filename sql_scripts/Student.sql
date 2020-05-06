386d0d506a004050eb73d94131c976d087cdafad
schmidtb2779 <schmidtb2779@my.uwstout.edu>
2020-05-06 17:51:49 -0500

    Student data script

---
+++ sql_scripts/Student.sql
@@ -0,0 +1,6 @@
+INSERT INTO `G7AgileExperience`.`EnrollsIN` (`idStudent`, `fullName`) VALUES ('12345', 'TEST Student 1');
+INSERT INTO `G7AgileExperience`.`EnrollsIN` (`idStudent`, `fullName`) VALUES ('12453', 'TEST Student 2');
+INSERT INTO `G7AgileExperience`.`EnrollsIN` (`idStudent`, `fullName`) VALUES ('12561', 'TEST Student 3');
+INSERT INTO `G7AgileExperience`.`EnrollsIN` (`idStudent`, `fullName`) VALUES ('12669', 'TEST Student 4');
+INSERT INTO `G7AgileExperience`.`EnrollsIN` (`idStudent`, `fullName`) VALUES ('12777', 'TEST Student 5');
+INSERT INTO `G7AgileExperience`.`EnrollsIN` (`idStudent`, `fullName`) VALUES ('12885', 'TEST Student 6');
