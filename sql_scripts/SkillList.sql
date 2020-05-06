649988d734c4dde523bbd422fcd2368f74c1c53c
schmidtb2779 <schmidtb2779@my.uwstout.edu>
2020-05-06 17:49:52 -0500

    SkillList data script

---
+++ sql_scripts/SkillList.sql
@@ -0,0 +1,3 @@
+INSERT INTO `G7AgileExperience`.`SkillList` ('skillListID', 'siID') VALUES ('1','18');
+INSERT INTO `G7AgileExperience`.`SkillList` ('skillListID', 'siID') VALUES ('2','29');
+INSERT INTO `G7AgileExperience`.`SkillList` ('skillListID', 'siID') VALUES ('3','117');
