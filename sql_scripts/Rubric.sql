0c288b3c9d67fff7cadbfade2999df55f5c9dded
schmidtb2779 <schmidtb2779@my.uwstout.edu>
2020-05-06 17:40:34 -0500

    Rubric data script

---
+++ sql_scripts/Rubric.sql
@@ -0,0 +1,5 @@
+INSERT INTO `G7AgileExperience`.`Rubric` (`rubricID`, `maxPoints`) VALUES ('1', '100');
+INSERT INTO `G7AgileExperience`.`Rubric` (`rubricID`, `maxPoints`) VALUES ('2', '50');
+INSERT INTO `G7AgileExperience`.`Rubric` (`rubricID`, `maxPoints`) VALUES ('3', '100');
+INSERT INTO `G7AgileExperience`.`Rubric` (`rubricID`, `maxPoints`) VALUES ('4', '75');
+INSERT INTO `G7AgileExperience`.`Rubric` (`rubricID`, `maxPoints`) VALUES ('5', '100');
