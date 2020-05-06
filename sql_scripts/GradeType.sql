4ee72a3f2e682855dcea93844350950675610f63
schmidtb2779 <schmidtb2779@my.uwstout.edu>
2020-05-06 17:35:48 -0500

    GradeType data script

---
+++ sql_scripts/GradeType.sql
@@ -0,0 +1,3 @@
+INSERT INTO `G7AgileExperience`.`GradeType` (`ID`, 'TypeOfGrade') VALUES ('1', 'RangedScore');\
+INSERT INTO `G7AgileExperience`.`GradeType` (`ID`, 'TypeOfGrade') VALUES ('2', 'In/Complete');
+INSERT INTO `G7AgileExperience`.`GradeType` (`ID`, 'TypeOfGrade') VALUES ('3', 'ByRubric');
